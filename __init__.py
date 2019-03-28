f7fc141742a9861c91d296e41577e5b8c24e6144
Ben Yang <benayang@gmail.com>
2019-03-28 12:42:53 -0700

    rearranged files

---
+++ __init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import sqlite3
+from pathlib import Path
+
+conn = sqlite3.connect(Path(__file__).parent/"ids-data.db")
+cursor = conn.cursor()
+
+def find(components):
+    ideostr = f"{tuple(components)}" if len(components) > 1 else f"('{components}')"
+    cursor.execute(f"SELECT ids FROM ids_data WHERE ideo in {ideostr}")
+    try:
+        r = cursor.fetchone()[0]
+    except TypeError:
+        return set()
+    s = set(r)
+    output = s | {components[0]}
+    for r, ideo in zip(cursor.fetchall(), components[1:]):
+        s = set(r[0])
+        output &= (s | {ideo})
+    return output
+
+if __name__ == "__main__":
+    components = sys.argv[1]
+    output = find(components)
+    sys.stdout.write("".join(sorted(output)))
+    conn.close()
+    sys.exit(0)
