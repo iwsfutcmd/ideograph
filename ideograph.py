3a0c31f4b3a788e37278e7d18e45d17f89038de1
iwsfutcmd <benayang@gmail.com>
2019-03-28 05:01:09 -0700

    fixed single-character input

--- ideograph.py
+++ ideograph.py
@@ -4,7 +4,8 @@ import sys
 import sqlite3
 
 def sqlite_get(cursor, ideos):
-    cursor.execute(f"SELECT ids FROM ids_data WHERE ideo in {tuple(ideos)}")
+    ideostr = f"{tuple(ideos)}" if len(ideos) > 1 else f"('{ideos}')"
+    cursor.execute(f"SELECT ids FROM ids_data WHERE ideo in {ideostr}")
     r = cursor.fetchone()[0]
     s = set(r)
     output = s | {ideos[0]}
@@ -15,7 +16,6 @@ def sqlite_get(cursor, ideos):
 
 if __name__ == "__main__":
     conn = sqlite3.connect("ids-data.db")
-    #conn.set_trace_callback(print)
     cursor = conn.cursor()
     ideos = sys.argv[1]
     output = sqlite_get(cursor, ideos)
